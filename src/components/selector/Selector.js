57a61d905aacd605f0ca7151225b5ae654f533d7
ab.reza <a.re.morteza@gmail.com>
2020-05-31 03:10:22 +0430

    fix select issues add add_new mode select add multiple select

--- src/components/selector/Selector.js
+++ src/components/selector/Selector.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
-import { Input } from 'semantic-ui-react';
+import { Input, Icon } from 'semantic-ui-react';
 
 import SelectorList from './SelectList';
 
 import '../../styles/Selector.css';
-import './selector-client'
+import './selector-client';
 
 export default class Selector extends Component {
   render() {
+    let selected_lables = '';
+    if (this.props.multiple) {
+      let labels_list = this.props.items.map(function (item, index) {
+        return (
+          <li data-id={index}>
+            {' ' + item.name + ' '}
+          </li>
+        );
+      });
+      selected_lables = <ul className="selected-labels">{labels_list}</ul>;
+    }
+
     return (
-      <div class="selector">
-        <Input placeholder="منبع" type="text" className="rtl" />
-        <SelectorList />
+      <div className={this.props.multiple ? 'selector multiple' : 'selector'}>
+        {this.props.add_new ? <small className="new-source">جدید</small> : ''}
+        <div>
+          <Input
+            placeholder={this.props.placeholder}
+            type="text"
+            className="rtl"
+          />
+          <Icon disabled className="search-icon" name="search" />
+          {selected_lables}
+        </div>
+        <SelectorList items={this.props.items} />
       </div>
     );
   }
