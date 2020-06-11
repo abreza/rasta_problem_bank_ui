435508a1d474c1325c07a1307dd465c5213d418e
AmooHashem <amoo.hashem.mehraban@gmail.com>
2020-06-12 03:51:26 +0430

    create model of bank

---
+++ src/model.js
@@ -0,0 +1,49 @@
+let rasta_bank = {
+
+  questionProperties: {
+    tags: [
+      {
+        id: 1,
+        name: 'ترکیبیات',
+        subtags: ['استقرا', 'لانه کبوتری'],
+      },
+      {
+        id: 2,
+        name: 'هندسه',
+        subtags: ['مسطحه', 'فضایی'],
+      },
+    ],
+    events: ['مدرسه تابستانه', 'کابارآمادالاپسته'],
+    sources: ['المپیاد ریاضی روسیه', 'سایت بریلیانت'],
+    authors: ['عرفان معینی', 'رضا ابوالقاسمی'],
+  },
+
+  questions: [
+    {
+      info: {
+        id: 123,
+        name: '',
+        author: '',
+        tags: [],
+        subTags: [],
+        events: [],
+        source: '',
+        hardness: {
+          hardnessValue: 5,// همونی که از ۱۰عه
+          leastAppropriateGrade: 9,
+          lastAppropriateGrade: 12,
+        },
+        reviewStatus: '', // وضعیت بازبینی سوال توسط منتورهای بالاسری 
+      },
+      questionText: '',
+      questionAnswer: '',
+    },
+  ],
+
+  account: {
+    isLoggedIn: false,
+    user: {
+      type: 'STUDENT',
+    },
+  },
+};
