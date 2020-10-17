9c5ac729e3bdbab3f076dbc645281c92c8f99deb
amooHashem <amoo.hashem.mehraban@gmail.com>
2020-10-17 12:36:52 +0330

    fix dev/prod

--- src/root/Root.prod.js
+++ src/root/Root.prod.js
@@ -1,33 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import LoginPage from '../containers/Login';
 import RegistrationPage from '../containers/Registration';
-import Homepage from '../containers/Homepage';
-import QuestionView from '../containers/QuestionView';
-import Question from '../containers/Problem';
-import AccountsRating from '../containers/AccountsRating';
+import Homepage from '../containers/HomePage';
+import ViewProblem from '../containers/ViewProblem';
+import Problem from '../containers/Problem';
 import ProblemSet from '../containers/ProblemSet';
-
+import UsersRating from '../containers/UsersRating';
 import NavBar from '../components/NavBar/NavBar';
 import NavBarItems from '../components/NavBar/NavBarItems';
 
 import '../styles/App.css';
 import PrivateRoute from './PrivateRoute';
+import { logout } from '../redux/actions/account'
 
-const Root = () => (
-  <div>
-    <NavBar config={NavBarItems({ loggedIn: false })}>
-      <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/registration" component={RegistrationPage} />
-        <PrivateRoute path="/question/:id" component={QuestionView} />
-        <PrivateRoute path="/edit_question/:id" component={Question} />
-        <PrivateRoute path="/rating" component={AccountsRating} />
-        <PrivateRoute path="/problemset" component={ProblemSet} />
-        <Route path="/" component={Homepage} />
-      </Switch>
-    </NavBar>
-  </div>
-);
-export default Root;
+
+
+class Root extends Component {
+
+  render() {
+    return (
+      <div>
+        <NavBar config={NavBarItems({
+          isLoggedIn: this.props.isLoggedIn,
+          username: this.props.username,
+          logout: this.props.logout,
+        })}>
+          <Switch>
+            <Route path="/login" component={LoginPage} />
+            <Route path="/registration" component={RegistrationPage} />
+            <Route path="/problem/:id" component={ViewProblem} />
+            <PrivateRoute path="/make_problem/" component={Problem} />
+            <PrivateRoute path="/edit_problem/:id" component={Problem} />
+            <PrivateRoute path="/problemset" component={ProblemSet} />
+            <PrivateRoute path="/users_rating" component={UsersRating} />
+            <Route path="/" component={Homepage} />
+          </Switch>
+        </NavBar>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  isLoggedIn: state.account.isLoggedIn,
+  username: state.account.username,
+})
+
+
+
+export default connect(mapStateToProps, { logout })(Root);
