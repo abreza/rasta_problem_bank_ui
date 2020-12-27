cb7ee8eca4428440ef40e0a2570d136fe2ba5333
amoohashem <amoo.hashem.mehraban@gmail.com>
2020-12-28 02:58:54 +0330

    erase local storage

--- src/index.js
+++ src/index.js
@@ -5,15 +5,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import configureStore from './redux/store/configureStore';
 
-const persistedState = localStorage.getItem('ProblemBank')
-  ? JSON.parse(localStorage.getItem('ProblemBank'))
+const persistedState = localStorage.getItem('ProblemBank1')
+  ? JSON.parse(localStorage.getItem('ProblemBank1'))
   : {};
 
 const store = configureStore(persistedState);
 
 store.subscribe(() => {
   localStorage.setItem(
-    'ProblemBank',
+    'ProblemBank1',
     JSON.stringify({
       account: { ...store.getState().account },
     })
